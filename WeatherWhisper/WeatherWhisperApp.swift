2040b6a9b83f95d502ff26b2bdbe1fc368d32de0
= <=>
2023-11-23 11:35:55 +0530

    Initial Commit

---
+++ WeatherWhisper/WeatherWhisperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWhisperApp.swift
+//  WeatherWhisper
+//
+//  Created by Nirmal Naveen on 2023-11-23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWhisperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
