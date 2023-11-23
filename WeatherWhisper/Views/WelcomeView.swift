851e6fb66599dff106d89918e0c546746aaa708f
= <=>
2023-11-23 12:50:37 +0530

    Update welcome UI

--- WeatherWhisper/Views/WelcomeView.swift
+++ WeatherWhisper/Views/WelcomeView.swift
@@ -6,12 +6,19 @@
 //
 
 import SwiftUI
+import CoreLocation
 
 struct WelcomeView: View {
     @EnvironmentObject var locationManager: LocationManager
     var body: some View {
         VStack {
-            
+            VStack(spacing: 20) {
+                Text("Welcome to the Weather Whisper ").bold().font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                
+                Text("Please share your current location to get the weather in your area.").padding()
+            }
+            .multilineTextAlignment(.center)
+            .padding()
         }
     }
 }
