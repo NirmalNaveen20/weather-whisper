39ade6354cb24a63a8ab5cf8404accd355e19627
= <=>
2023-11-23 12:26:29 +0530

    Add feature to get location from the users

--- WeatherWhisper/Manager/LocationManager.swift
+++ WeatherWhisper/Manager/LocationManager.swift
@@ -6,5 +6,32 @@
 //
 
 import Foundation
+// Get current location of users
 import CoreLocation
 
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    
+    @Published var location: CLLocationCoordinate2D?
+    @Published var isLoading = false
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestLocation() {
+        isLoading = true
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+        isLoading = false
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error getting location", error)
+        isLoading = false
+    }
+}
